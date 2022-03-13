88b9a2db970f5d264159b154b9c528bc64089d3f
Alexandro M <71974764+andomatthew@users.noreply.github.com>
2022-03-13 08:14:51 +0800

    initializing project

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
+import { useEffect } from 'react'
+
 import '../styles/globals.css'
+import '../styles/scss/global.scss' //bootstrap
+
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap")
+  })
+
   return <Component {...pageProps} />
 }
 
